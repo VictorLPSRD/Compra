04468b401b097f2fa2e79c9e2ebc943ba4535750
Victor Lopes <125214784+VictorLPSRD@users.noreply.github.com>
2024-11-22 01:34:45 -0300

    Update Listar.java

--- compra/Listar.java
+++ compra/Listar.java
@@ -1,14 +1,14 @@
 package compra;
 
 public class Listar {
-    // Método para listar os produtos do carrinho
+
     public static void listarProdutos(String[] produtos, Float[] valores) {
         if (produtos.length == 0) {
             System.out.println("O carrinho está vazio.");
         } else {
             System.out.println("Produtos no carrinho:");
             for (int i = 0; i < produtos.length; i++) {
-                // Para cada produto, imprime o nome e o valor correspondente
+          
                 System.out.println("Nome: " + produtos[i] + " | Valor: " + valores[i]);
             }
         }
